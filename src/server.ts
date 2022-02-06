7049011c70b08e96a37aa3ad94af4cf99e1158da
Pedro Gomes <pgsantanadesouza@usp.br>
2022-02-05 23:04:20 -0300

    feat: server started with appointments repository

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import express from "express";
+import router from "./routes";
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  return res.status(200).json({ message: "Hello world" });
-});
+app.use(router);
 
 app.listen(3333, () => {
   console.log("Server started on port 3333");
